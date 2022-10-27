a751c62c13f7884e36debbc5e9a95c1e0d21c59a
pablo.jorda <pablo.jorda@cognizant.com>
2022-10-27 13:30:04 +0200

    react example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { StateMachine } from 'react-machinery';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/*
+  This example uses a regular react component as the data store for our state machine.
+  Updating the state is as simple as passing a function that calls setState on our component.
+  At any time, we can be in one of four states:
+    - theNumberOne
+    - theNumberTwo
+    - theNumberTen
+    - lifeTheUniverseAndEverything
+  We can only transition to some states if we are already in a certain state. For example,
+  'theNumberTen' can only be transitioned if we are already in 'theNumberTwo'.
+*/
+
+const One = () => <div>State #1</div>
+const Two = () => <div>State #2</div>
+const Ten = () => <div>State #10</div>
+const HitchHikerComponent = () => <div>Theres a frood who really knows where his towel is.</div>
+
+const states = [
+  {
+    name: 'theNumberOne',
+    autoTransitions: [
+      {
+        test: ({n}) => n === 2,
+        newState: 'theNumberTwo'
+      }
+    ],
+    component: One
+  },
+  {
+    name: 'theNumberTwo',
+    autoTransitions: [
+      {
+        test: ({n}) => n === 1,
+        newState: 'theNumberOne'
+      },
+      {
+        test: ({n}) => n === 10,
+        newState: 'theNumberTen'
+      },
+    ],
+    component: Two
+  },
+  {
+    name: 'theNumberTen',
+    autoTransitions: [
+      {
+        test: ({n}) => n === 1,
+        newState: 'theNumberOne'
+      },
+      {
+        test: ({n}) => n === 42,
+        newState: 'lifeTheUniverseAndEverything'
+      }
+    ],
+    component: Ten
+  },
+  {
+    name: 'lifeTheUniverseAndEverything',
+    component: HitchHikerComponent
+  },
+];
+
+export default class App extends Component {
+  state = {
+    n: 1,
+    stateName: 'theNumberOne'
+  };
+
+  render() {
+    const { n, stateName } = this.state;
+    return <div>
+      <StateMachine
+        getCurrentState={() => stateName}
+        setNewState={stateName => this.setState(() => ({ stateName }))}
+        data={{n}}
+        states={states}
+      />
+
+      <div>{n}</div>
+
+      <button onClick={() => this.setState(() => ({n: n + 1}))}>+1</button>
+      <button onClick={() => this.setState(() => ({n: n + 10}))}>+10</button>
+      <button onClick={() => this.setState(() => ({n: n - 1}))}>-1</button>
+      <button onClick={() => this.setState(() => ({n: n - 10}))}>-10</button>
+    </div>;
+  }
 }
-
-export default App;
